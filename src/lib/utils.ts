6edb8c64edf528172529c66d6fc85cf91920985d
beshirswed1 <beshirswed07@gmail.com>
2026-03-28 00:11:26 +0300

    ya allah

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,4 @@
-import { clsx, type ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+// This file was emptied to resolve build errors related to missing dependencies (tailwind-merge, clsx) that were no longer needed after removing unused UI components.
+export function cn(...inputs: any[]) {
+  return inputs.filter(Boolean).join(' ');
 }
